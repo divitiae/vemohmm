8b2a9a5a3cc263f3731399338913e9823a9a53bb
Mohmmad <setc.email@gmail.com>
2020-08-08 18:12:06 -0400

    Add all my files

---
+++ app/src/main/java/com/divitiae/videoeditor/phototovideo/tablayout/Universal.java
@@ -0,0 +1,10 @@
+package com.divitiae.videoeditor.phototovideo.tablayout;
+
+import androidx.viewpager.widget.ViewPager;
+
+import com.divitiae.videoeditor.phototovideo.tablayout.HomeTab.TabPagerAdapter;
+
+public class Universal {
+    public static TabPagerAdapter fadt;
+    public static ViewPager vobj;
+}
---
+++ app/src/main/java/com/divitiae/videoeditor/videocollage/utils/Utils.java
@@ -0,0 +1,17 @@
+package com.divitiae.videoeditor.videocollage.utils;
+
+import com.divitiae.videoeditor.videocollage.model.BorderAttribute;
+import com.divitiae.videoeditor.videocollage.model.CollageData;
+import com.divitiae.videoeditor.videocollage.stickers.StickerData;
+
+import java.util.ArrayList;
+
+public class Utils {
+    public static ArrayList<BorderAttribute> borderParam = new ArrayList<>();
+    public static ArrayList<BorderFrameLayout> borderlayout = new ArrayList<>();
+    public static ArrayList<StickerData> clgstickerviewsList = new ArrayList<>();
+    public static ArrayList<CollageData> collageData = new ArrayList<>();
+    public static int position = 0;
+    public static int selectedPos = 0;
+    public static String selectedText = "";
+}
